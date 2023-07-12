4c692afd040c14607bc5c9609ae6d89636b58457
Stg8604 <sashaankram@gmail.com>
2023-07-13 01:31:37 +0530

    PLEASE2

--- server.py
+++ server.py
@@ -2,7 +2,9 @@ from fastapi import FastAPI, Depends,  HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from pydantic import BaseModel
 from datetime import datetime,timedelta
-from jose import JWTError,jwt
+#from jose import JWTError,jwt
+import jwt
+from jwt import JWTError
 from passlib.context import CryptContext
 import mysql.connector
 from mysql.connector import connect, Error
