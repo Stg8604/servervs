5725ade1b88b46ba27e73a84910efd496dcdac67
Stg8604 <sashaankram@gmail.com>
2023-07-13 01:08:32 +0530

    second commit

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Depends,  HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from pydantic import BaseModel
 from datetime import datetime,timedelta
+import jose
 from jose import JWTError,jwt
 from passlib.context import CryptContext
 import mysql.connector
